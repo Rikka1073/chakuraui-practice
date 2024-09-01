bb752aeebf4c0f8a0e9a52bc4c5cb74ccf14fb0d
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-09-01 21:36:56 +0900

    ユーザー一覧表示

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,32 @@
-import { FC, memo } from "react";
+import { FC, memo, useEffect } from "react";
 import { HeaderLayout } from "../templates/HeaderLayout";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
 import { UserCard } from "../organisms/layout/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
     <>
       <HeaderLayout children />
-      <Wrap p={{ base: 4, md: 10 }}>
-        <WrapItem>
-          <UserCard imageUrl="https://picsum.photos/800" userName="トーマス" fullName="Totia Masafumi" />
-        </WrapItem>
-      </Wrap>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => {
+            return (
+              <WrapItem key={user.id} mx="auto">
+                <UserCard imageUrl="https://picsum.photos/800" userName={user.username} fullName={user.name} />
+              </WrapItem>
+            );
+          })}
+        </Wrap>
+      )}
     </>
   );
 });
