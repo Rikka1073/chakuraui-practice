d9568e84d861d994b16a3fccbc81741e63688bd0
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-09-01 13:36:44 +0900

    ログイン成功時、失敗時のメッセージを追加

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { User } from "../types/api/user";
 import { useNavigate } from "react-router-dom";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const navigate = useNavigate();
+  const { showMessage } = useMessage();
   const [loading, setLoding] = useState(false);
   const login = useCallback(
     (id: string) => {
@@ -14,20 +16,21 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
             navigate("/home");
           } else {
-            alert("ユーザーが見つかりません");
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
           }
         })
         .catch(() => {
-          alert("ログインできません");
+          showMessage({ title: "ログインできません", status: "error" });
         })
         .finally(() => {
           setLoding(false);
         });
       return { login };
     },
-    [navigate]
+    [navigate, showMessage]
   );
   return { login, loading };
 };
