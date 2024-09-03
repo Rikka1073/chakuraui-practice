3374e73d3bd3c3e6959f898aa3381a8e9078bb77
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-09-03 22:13:08 +0900

    管理者設定

--- src/provider/LoginUserProvider.tsx
+++ src/provider/LoginUserProvider.tsx
@@ -2,16 +2,18 @@ import { User } from "../types/api/user";
 import { createContext, Dispatch, SetStateAction, useState } from "react";
 import { ReactNode } from "react";
 
+type LoginUser = User & { isAdmin: boolean };
+
 export type LoginUserContextType = {
   loginUser: User | null;
-  setLoginUser: Dispatch<SetStateAction<User | null>>;
+  setLoginUser: Dispatch<SetStateAction<LoginUser | null>>;
 };
 
 export const LoginUserContext = createContext<LoginUserContextType>({} as LoginUserContextType);
 
 export const LoginUserProvider = (props: { children: ReactNode }) => {
   const { children } = props;
-  const [loginUser, setLoginUser] = useState<User | null>(null);
+  const [loginUser, setLoginUser] = useState<LoginUser | null>(null);
   return (
     <>
       <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>{children}</LoginUserContext.Provider>
